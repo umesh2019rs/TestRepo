8832c07f13fa296dad9d7e56883979d528380909
Umesh BR <umesh.br@robosoftin.com>
2020-02-17 16:50:59 +0530

    delete patch

--- app/src/main/java/com/example/demokotlin/SplashActivity.kt
+++ app/src/main/java/com/example/demokotlin/SplashActivity.kt
@@ -14,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
     // New changes form myside
     // check new bragit nch 2
     // check patch 2
+    // delete patch
     private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
