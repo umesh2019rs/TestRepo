885a786128041e4e2935c0db2c6afa2a075219b1
Umesh BR <umesh.br@robosoftin.com>
2020-02-17 14:37:25 +0530

    first commit of projest

---
+++ app/src/main/java/com/example/demokotlin/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.demokotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.example.demokotlin.Activities.ui.login.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    // This is the loading time of the splash screen
+    private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this , LoginActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+
+    }
+}
