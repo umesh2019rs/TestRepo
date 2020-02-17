df9c7abafdf4adaa2b43edc1bf3990307a858faa
Umesh BR <umesh.br@robosoftin.com>
2020-02-17 18:50:57 +0530

    delete new

--- app/src/main/java/com/example/demokotlin/SplashActivity.kt
+++ app/src/main/java/com/example/demokotlin/SplashActivity.kt
@@ -20,7 +20,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //New changes from myside
         Handler().postDelayed({
             startActivity(Intent(this , LoginActivity::class.java))
             finish()
