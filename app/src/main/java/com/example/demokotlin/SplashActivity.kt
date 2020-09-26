860638ebddd2d97e37a8fb7aa8c513a971632b44
Umesh BR <59826993+umesh2019rs@users.noreply.github.com>
2020-09-26 13:13:16 +0530

    Update SplashActivity.kt
    Deleted all commits

--- app/src/main/java/com/example/demokotlin/SplashActivity.kt
+++ app/src/main/java/com/example/demokotlin/SplashActivity.kt
@@ -8,14 +8,13 @@ import com.example.demokotlin.Activities.ui.login.LoginActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    //Delete all comments
-    //add comment1 in master branch
+ 
     private val SPLASH_TIME_OUT:Long = 3000 // 1 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //New changes from myside
+     
         Handler().postDelayed({
             startActivity(Intent(this , LoginActivity::class.java))
             finish()
