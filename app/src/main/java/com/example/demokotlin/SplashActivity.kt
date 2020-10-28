a0948006822888a3b2309c1ebc02f2ce97b5d0e1
Umesh BR <umesh.br@robosoftin.com>
2020-10-28 17:46:12 +0530

    Added comment in feature_2

--- app/src/main/java/com/example/demokotlin/SplashActivity.kt
+++ app/src/main/java/com/example/demokotlin/SplashActivity.kt
@@ -11,6 +11,7 @@ class SplashActivity : AppCompatActivity() {
 
     //Added comment in master 2
     //Added cooment in master 1
+    //Added comment in feature_2
     //Added comment in master
     //Added comment in feature_1
     private val SPLASH_TIME_OUT:Long = 3000
